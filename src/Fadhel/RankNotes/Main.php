6c0b70da5cf10670c02ed197ec77ee80e27e1bb0
BLAST1718 <71514704+BLAST1718@users.noreply.github.com>
2020-11-16 16:01:51 +0800

    Update Main.php

--- src/Fadhel/RankNotes/Main.php
+++ src/Fadhel/RankNotes/Main.php
@@ -10,9 +10,9 @@ class Main extends PluginBase
     public $rank;
     public function onEnable()
     {
-        $this->rank = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+        $this->rank = $this->getServer()->getPluginManager()->getPlugin("TempRanks");
         if($this->rank === null){
-            $this->getLogger()->critical("PurePerms plugin not found!");
+            $this->getLogger()->critical("TempRanks plugin not found!");
             $this->setEnabled(false);
             return;
         }
